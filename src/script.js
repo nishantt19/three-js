8c8d27ad3ec9cb4bc578c9d3e4481f773852d1db
Nishant <108404252+nishantt19@users.noreply.github.com>
2022-12-29 22:28:18 +0530

    transforming objects

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import './style.css';
 
 import * as THREE from 'three';
+import { Mesh } from 'three';
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,18 +16,39 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: '#ff0000'
-})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
+// Group of cubes
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color : 0xff0000})
+)
+group.add(cube1);
+
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color : 0x00ff00})
+)
+cube2.position.x= -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color : 0x0000ff})
+)
+cube3.position.x= +2;
+group.add(cube3);
+
+
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -34,3 +56,29 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+
+
+
+
+// Transforming objects properties 
+// position
+// cubeMesh.position.x = 1;
+// cubeMesh.position.y = 1;
+// cubeMesh.position.z = 0.5;
+// cubeMesh.position.set(1, 0.4, 0.6);
+
+// scale
+// cubeMesh.scale.x = 2;
+// cubeMesh.scale.y = 0.5;
+// cubeMesh.scale.z = 0.5;
+// cubeMesh.scale.set(2, 0.5, 0.5);
+
+// Axes helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
+// rotation
+// cubeMesh.rotation.x = 2;
+// cubeMesh.rotation.y = 2;
+// cubeMesh.rotation.z = 2;
